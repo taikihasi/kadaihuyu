dd149f31d898cad815e4a155dd34841e4021fd0b
hasimoto <0zd19w2z6mb3-1s@au.com>
2023-01-11 17:24:12 +0900

    フィールド、ブロック

--- main.cpp
+++ main.cpp
@@ -4,10 +4,11 @@
 #include <time.h>
 #include <conio.h>
 
-//サイズ
-static const int FIELD_WIDTH = 12;
-static const int FIELD_HEIGHT = 22;
-
-static const int MINO_WIDTH = 4;
-static const int MINO_HEIGHT = 4;
-
+// フィールド及びブロックの設定
+#define F_WIDTH  12               // フィールドの幅
+#define F_HEIGHT 21               // フィールドの高さ
+#define F_NUM  F_WIDTH * F_HEIGHT // フィールドのマスの数
+#define NO_WALL_NUM  (F_WIDTH - 2) * (F_HEIGHT - 1) //左右下の壁を除いた時のフィールドのマスの数
+#define B_WIDTH  4                // ブロックの幅
+#define B_HEIGHT 4                // ブロックの高さ
+#define B_NUM  B_WIDTH * B_HEIGHT // ブロックのマスの数
