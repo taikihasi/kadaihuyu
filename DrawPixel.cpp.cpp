7e1d36e93a0dc1808d4e7db5d89abf4bf3e24042
hasimoto <0zd19w2z6mb3-1s@au.com>
2023-01-11 13:52:09 +0900

    プロジェクト ファイルを追加します。

---
+++ DrawPixel.cpp.cpp
@@ -0,0 +1,13 @@
+#include "DxLib.h"
+#include <iostream>
+#include <stdlib.h>
+#include <time.h>
+#include <conio.h>
+
+//ÉTÉCÉY
+static const int FIELD_WIDTH = 12;
+static const int FIELD_HEIGHT = 22;
+
+static const int MINO_WIDTH = 4;
+static const int MINO_HEIGHT = 4;
+
